88ecf8178b5adb7a3f2dfa0432ed2de99fdca2f0
Flick (Alex) <flameflick@pm.me>
2023-01-06 14:34:45 +0300

    fix(utils): id check

--- src/utils/beatsaver.ts
+++ src/utils/beatsaver.ts
@@ -2,5 +2,7 @@
 const BEATSABER_LEVEL_ID_RE = /^custom_level_(.*)$/
 
 export const beatSaberLevelIdToHash = (id: string) => {
+    if (!id) return null
+
     return id.match(BEATSABER_LEVEL_ID_RE)![1]
 }
