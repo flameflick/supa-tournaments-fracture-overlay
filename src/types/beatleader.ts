e2b723a13a43d6855f168411bf23974e2bc54916
Flick (Alex) <flameflick@pm.me>
2023-01-11 22:11:12 +0300

    feat(types): user providers

---
+++ src/types/beatleader.ts
@@ -0,0 +1,11 @@
+export interface IBeatLeaderUser {
+    id: string,
+    name: string,
+    platform: string,
+    avatar: string,
+    country: string,
+    pp: number,
+    rank: number,
+    countryRank: number,
+    role: string
+}
---
+++ src/types/scoresaber.ts
@@ -0,0 +1,8 @@
+export interface IScoreSaberBasicPlayer {
+    id: string
+    name: string
+    profilePicture: string
+    country: string
+
+    rank: number
+}
